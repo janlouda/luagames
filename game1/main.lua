606303268f923c6d78ca059237d3fa54e6542ee6
janlouda <louda.jan@email.cz>
2022-11-08 22:06:43 +0100

    initial commit

---
+++ game1/main.lua
@@ -0,0 +1,41 @@
+function love.load()
+	target = {}
+	target.x = 330
+	target.y = 330
+	target.radius = 50
+
+	score = 0
+	timer = 0
+
+	gameFont = love.graphics.setNewFont(40)
+end
+
+
+function love.update(dt)
+
+end
+
+
+function love.draw()
+	love.graphics.setColor(1, 0, 0)
+	love.graphics.circle("fill", target.x, target.y, target.radius)
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.setFont(gameFont)
+	love.graphics.print(score, 10,10)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+	if button == 1 then
+		local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+		if mouseToTarget < target.radius then
+			score = score + 1
+		end
+	end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+	return math.sqrt((x2-x1)^2 + (y2-y1)^2)
+end
+
+
+
